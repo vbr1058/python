953536bacc9101e4dd054d48f7c6770dd2d04035
vbr1058 <43924856+vbr1058@users.noreply.github.com>
2020-05-16 23:45:03 +0530

    Update luhn.py

--- exercises/luhn/luhn.py
+++ exercises/luhn/luhn.py
@@ -1,6 +1,18 @@
-class Luhn:
-    def __init__(self, card_num):
-        pass
-
-    def valid(self):
-        pass
+string="8273 1232 7352 0569"
+list1=string.split(" ")
+list2=[]
+sum=0
+for i in range(len(list1)):
+	num=list1[i]
+	for j in num:
+		list2.append(j)
+for i in range(len(list2)):
+	if(i%2==0):
+		val=int(list2[i])+int(list2[i])
+		if(val>9):
+			val=val-9
+		list2[i]=str(val)
+print(list2)
+for i in list2:
+	sum=sum+int(i)
+print(sum)
